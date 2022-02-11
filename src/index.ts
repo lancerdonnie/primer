96bd8aab598f9cd69cd2cb573e0e699aadf2a98b
lancerdonnie <jideadedejifirst@gmail.com>
2022-02-11 15:38:22 +0100

    feat: add handler

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-export {};
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import type { APIGatewayProxyEvent, Handler } from 'aws-lambda';
+
+const client = new DynamoDBClient({});
+
+const headers = {
+  'Content-Type': 'application/json',
+};
+
+export const prime: Handler = async (event: APIGatewayProxyEvent, context) => {
+  try {
+    // body = await dynamo
+    //   .scan({ TableName: event.queryStringParameters.TableName })
+    //   .promise();
+    // break;
+    // body = await dynamo.put(JSON.parse(event.body)).promise();
+    // break;
+  } catch (err) {}
+
+  return {
+    statusCode: 200,
+    body: { hello: 'hello world' },
+    headers,
+  };
+};
