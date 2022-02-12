0e1ecb0459715c5ecec8d39c9c76089894a6d8ef
lancerdonnie <jideadedejifirst@gmail.com>
2022-02-12 21:28:22 +0100

    fix: add trycatch to cache functions

--- backend/src/repo/prime.ts
+++ backend/src/repo/prime.ts
@@ -5,23 +5,31 @@ import { marshall, unmarshall } from '@aws-sdk/util-dynamodb';
 import { TABLE } from '../constants_';
 
 const getCachedResult = async (number: number): Promise<IPrime | undefined> => {
-  const { Item } = await db.send(
-    new GetItemCommand({
-      TableName: TABLE,
-      Key: marshall({ number }),
-    })
-  );
+  try {
+    const { Item } = await db.send(
+      new GetItemCommand({
+        TableName: TABLE,
+        Key: marshall({ number }),
+      })
+    );
 
-  return Item ? (unmarshall(Item) as IPrime) : undefined;
+    return Item ? (unmarshall(Item) as IPrime) : undefined;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const cacheResult = async (input: IPrime) => {
-  await db.send(
-    new PutItemCommand({
-      TableName: TABLE,
-      Item: marshall(input),
-    })
-  );
+  try {
+    await db.send(
+      new PutItemCommand({
+        TableName: TABLE,
+        Item: marshall(input),
+      })
+    );
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export { getCachedResult, cacheResult };
